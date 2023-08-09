fca1481f55368ecc114729eeabeffb7b11dee25f
annab77 <141236453+annab77@users.noreply.github.com>
2023-08-09 22:34:52 +0300

    finishing Main page
    finishing Main page

--- controller/category.controller.js
+++ controller/category.controller.js
@@ -3,12 +3,29 @@ const Categories = require("../model/Categories");
 exports.getCategory = async (req, res, next) => {
   try {
     const categoriesRespose = await Categories.find({});
+
     res.render("index", {
       categories: categoriesRespose,
-      pageTitle: "main",
-      path: "/",
+      pageTitle: "Home",
     });
   } catch (e) {
+    next(e);
+  }
+};
+
+exports.getOneCategory = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const categoriesRespose = await Categories.find({});
+
+    const categoryRespose = await Categories.findOne({ id: id });
+    res.render("category", {
+      categories: categoriesRespose,
+      category: categoryRespose,
+      pageTitle: categoryRespose.page_title,
+    });
+  } catch (e) {
+    console.log(e);
     next(e);
   }
 };
--- routes/category.router.js
+++ routes/category.router.js
@@ -2,5 +2,6 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("../controller/category.controller");
 
 router.get("/", controller.getCategory);
+router.get("/categories/:id", controller.getOneCategory);
 
 module.exports = router;
